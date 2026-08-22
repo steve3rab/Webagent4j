ee3d052716a59b8aee6d6f57af8ec81465595125
steve3rab <130780043+steve3rab@users.noreply.github.com>
2026-08-22 10:47:38 +0200

    test(workflow): use a valid preflight trace

--- webagent4j-workflow/src/test/java/io/webagent4j/workflow/WorkflowExceptionContractTest.java
+++ webagent4j-workflow/src/test/java/io/webagent4j/workflow/WorkflowExceptionContractTest.java
@@ -26,7 +26,15 @@ class WorkflowExceptionContractTest {
                 new WorkflowResult(
                         new WorkflowId("contract-test"),
                         WorkflowStatus.FAILED,
-                        List.of(),
+                        List.of(
+                                new WorkflowStepResult(
+                                        new WorkflowStepId("step-1"),
+                                        WorkflowStepType.ASSIGN,
+                                        WorkflowStepStatus.NOT_RUN,
+                                        Optional.empty(),
+                                        Optional.empty(),
+                                        Optional.empty(),
+                                        Optional.empty())),
                         WorkflowOutputs.empty(),
                         Optional.of(failure));
 
