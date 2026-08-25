21649d33413e0a2851a2e73fb5c1da06af876c99
steve3rab <130780043+steve3rab@users.noreply.github.com>
2026-08-25 16:31:05 +0200

    fix/some issues

--- webagent4j-core/src/main/java/io/webagent4j/core/WebAgent.java
+++ webagent4j-core/src/main/java/io/webagent4j/core/WebAgent.java
@@ -4,7 +4,7 @@ package io.webagent4j.core;
 public final class WebAgent {
 
     /** Current API version. */
-    public static final String VERSION = "0.1.0-SNAPSHOT";
+    public static final String VERSION = "1.0.0-RC1";
 
     private WebAgent() {}
 
